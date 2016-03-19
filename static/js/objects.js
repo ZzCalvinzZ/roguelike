295aa5c117dc5a8cda9a74b76df27a39570bd403
Calvin Collins <calvinkcollins@gmail.com>
2016-03-19 12:01:44 -0400

    rework objects

--- static/js/objects.js
+++ static/js/objects.js
@@ -1,11 +1,23 @@
-var baseObject = {
-	solid: true,
-};
+function BaseObject(info) {
+	this.sprite = info.sprite;
+	this.info = {
+		solid: info.solid || true,
 
-var player = _.create(baseObject, { 
+	};
+};
+BaseObject.prototype = {
+	constructor: BaseObject,
+
+	draw: function () {
+
+	},
+
+}
+
+var player = new BaseObject({
 	sprite: new PIXI.Text('@', {'fill':'white', 'font':'17px Arial'}),
 });
 
-var stoneWall = _.create(baseObject, {
+var stoneWall = new BaseObject({
 	sprite: PIXI.Texture.fromImage('static/img/wall20.png'),
 });
