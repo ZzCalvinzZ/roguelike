89b1534e699216973e26f2f00669b0e18e06220a
Calvin Collins <calvinkcollins@gmail.com>
2016-06-26 19:30:27 -0400

    fix

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ from flask_assets import Environment, Bundle
 
 app = Flask(__name__)
 
-app.config['ASSETS_DEBUG'] = True
-
 app.config.from_object('settings.default')
 app.config.from_object('settings.local')
 
